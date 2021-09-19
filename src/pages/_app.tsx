b86f58d3a6aebd349085e71ede4a161085c04324
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-09-19 20:49:14 -0300

    Adicionado lib next-page-transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { ThemeProvider } from "styled-components";
 import Head from "next/head";
+import { PageTransition } from "next-page-transitions";
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/global";
+import PageTransitionStyle, { TIMEOUT } from "@UI/animations/pageTransitions";
 import theme from "styles/theme";
 import BaseTemplate from "templates/Base";
 import type { AppProps } from "next/app";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <Head>
@@ -13,8 +15,18 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <PageTransitionStyle />
         <BaseTemplate>
-          <Component {...pageProps} />
+          <PageTransition
+            timeout={TIMEOUT}
+            classNames="page-transition"
+            loadingComponent={<div>CARREGANDO...</div>}
+            loadingDelay={0}
+            loadingTimeout={0}
+            skipInitialTransition
+          >
+            <Component {...pageProps} key={router.route} />
+          </PageTransition>
         </BaseTemplate>
       </ThemeProvider>
     </>
