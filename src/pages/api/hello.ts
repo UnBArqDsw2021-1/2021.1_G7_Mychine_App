b3eb13ded0a0843b3950eaf016885f640ffa3d1c
Pedrok99 <pedrohlima20@gmail.com>
2021-09-23 21:51:07 -0300

    Adicionando prisma client para acesso aos dados do banco

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
 
 type Data = {
   name: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: "John Doe" });
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
 }
