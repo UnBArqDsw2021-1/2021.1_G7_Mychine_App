e7559f8c98b93ab99fc54090062aeb4b614cee6a
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-09-24 21:07:19 -0300

    Fix PrismaClient

--- src/pages/api/user/create.ts
+++ src/pages/api/user/create.ts
@@ -1,33 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from '@prisma/client';
-import sha3 from 'crypto-js/sha3';
+// @ts-ignore
+import { PrismaClient } from "@prisma/client";
+import sha3 from "crypto-js/sha3";
 
-type user = {
-  email: string
-  password: string
-  name: string,
-  phone: string
-  cpf: string
-}
+type User = {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+  cpf: string;
+};
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-
-
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
   if (req.method === "POST") {
     const prisma = new PrismaClient();
-    let user: user = { ...req.body }
+    const user: User = { ...req.body };
 
     user.password = sha3(user.password).toString();
 
     try {
       await prisma.user.create({ data: user });
     } catch (error) {
-      res.status(400).json({ 'error': 'email already exists' });
+      res.status(400).json({ error: "email already exists" });
       return;
     }
     res.status(200).json(user);
     return;
   }
-  res.status(405).json({ 'error': `cannot handle ${req.method} method` });
+  res.status(405).json({ error: `cannot handle ${req.method} method` });
 }
