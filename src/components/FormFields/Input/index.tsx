ee74fb81c97a8e53cebc85c4754f714109b6e796
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-09-27 06:11:54 -0300

    Refatorado Input para usar estilos do tema
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>
    Co-authored-by: Sayuck <nobrega.m.roberto@gmail.com>

--- src/components/FormFields/Input/index.tsx
+++ src/components/FormFields/Input/index.tsx
@@ -1,30 +1,43 @@
-import { InternalFieldErrors } from "react-hook-form";
+import { forwardRef, InputHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
+import { MdErrorOutline } from "react-icons/md";
 import * as S from "./styles";
 
-type InputProps = React.DetailedHTMLProps<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->;
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 export interface IInputProps extends InputProps {
+  name: string;
   label: string;
-  errors: InternalFieldErrors;
-  inputState?: "success" | "warning" | "info" | "error";
   icon?: string;
 }
 
-const Input = (props: IInputProps) => {
-  const { name, label, errors } = props;
+const Input = forwardRef<HTMLInputElement, IInputProps>((props, ref) => {
+  const { name, label, icon } = props;
+  const methods = useFormContext();
+
+  const error = methods?.formState?.errors[name];
 
   return (
     <S.InputWrapper>
       <S.InputGroup>
-        <S.Input id={name} {...props} />
+        <S.Input
+          id={name}
+          ref={ref}
+          {...methods?.register(name)}
+          {...props}
+          placeholder={label}
+          icon={icon || error}
+        />
         <S.Label htmlFor={name}>{label}</S.Label>
+        {error && (
+          <S.Icon>
+            <MdErrorOutline size={20} />
+          </S.Icon>
+        )}
       </S.InputGroup>
-      {errors && <span>{errors?.message}</span>}
+      {error && <S.Error>{error?.message}</S.Error>}
     </S.InputWrapper>
   );
-};
+});
 
 export default Input;
