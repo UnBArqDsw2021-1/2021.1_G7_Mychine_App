960d773c4b6ad588143633c0ef8f99f40ff37429
Samuel Nogueira <samuelnbacelar@hotmail.com>
2021-10-03 18:05:59 -0300

    arrumando bugs e add novo model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,5 @@
+import { AddresCreate } from './Address';
+
 export interface UserCreate {
   email: string;
   password: string;
@@ -11,4 +13,9 @@ export interface UserUpdate {
   name?: string;
   phone?: string;
   email?: string;
+}
+
+export interface CreateAccount {
+  User: UserCreate;
+  Address: AddresCreate;
 }
