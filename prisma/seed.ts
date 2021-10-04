0fd9bfe49067046ebd448e6f895259e76e19e05a
Pedrok99 <pedrohlima20@gmail.com>
2021-10-04 13:37:02 -0300

    fazendo Hashings das senhas durante o seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+import sha3 from 'crypto-js/sha3';
 
 import { address, category, products, users } from './seedData';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  for (let i = 0; i < users.length; i += 1) {
+    users[i].password = sha3(users[i].password).toString();
+  }
+
   await prisma.user.createMany({ data: users });
   await prisma.category.createMany({ data: category });
   await prisma.product.createMany({ data: products });
