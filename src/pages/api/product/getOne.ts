28a8ef7bad051d208385748ce137a948aa3e86cf
Samuel Nogueira <samuelnbacelar@hotmail.com>
2021-10-03 23:19:39 -0300

    Criando rota para buscar um produto por ID

---
+++ src/pages/api/product/getOne.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ProductController } from '@controllers/productController';
+
+// eslint-disable-next-line import/no-unresolved
+import prisma from '@prismadb/database';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method === 'GET') {
+    const productController = new ProductController();
+    if (!req.query.id) {
+      res.status(400).json({ error: 'missing id attribute in request URL' });
+      return;
+    }
+    let productId: any = req.query.id;
+    productId = parseInt(productId, 10);
+    const aProduct = await productController.getOne(prisma, {
+      where: {
+        productId,
+      },
+      include: {
+        category: true,
+      },
+    });
+
+    if (!aProduct) {
+      res.status(400).json({ error: `no product found with id ${productId}` });
+      return;
+    }
+    delete aProduct.categoryId;
+    res.status(200).json(aProduct);
+  }
+}
