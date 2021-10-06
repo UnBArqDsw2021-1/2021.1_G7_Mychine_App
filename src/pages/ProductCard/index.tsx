3f6051ec1d51d75393ffc93c5bdfa04423d506cc
igorq937 <igorq937@gmail.com>
2021-10-06 20:16:02 -0300

    Adicionado componente ProductCard
    Co-authored-by: Giovannabbottino <giovannabbottino@gmail.com>

---
+++ src/pages/ProductCard/index.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import * as S from './styles';
+
+export interface CardProps {
+  imgSrc: string;
+  year: string;
+  brand: string;
+  model: string;
+  dayPrice: string;
+  weekPrice: string;
+}
+
+const ProductCard = (props: CardProps) => {
+  const { year, brand, model, dayPrice, weekPrice, imgSrc } = props;
+
+  return (
+    <Link href="/" passHref>
+      <S.Card>
+        <S.Hero>
+          <Image
+            src={imgSrc}
+            width="100%"
+            height="80%"
+            layout="responsive"
+            objectFit="cover"
+          />
+        </S.Hero>
+        <S.Content>
+          <S.Year>
+            <span>Ano</span>
+            {year}
+          </S.Year>
+          <S.Model>
+            <span>{brand}</span>
+            {model}
+          </S.Model>
+          <S.Price>
+            <span>Aluguel</span>
+            {dayPrice}
+          </S.Price>
+          <S.Price>
+            <span>A partir de 10 dias</span>
+            {weekPrice}
+          </S.Price>
+        </S.Content>
+      </S.Card>
+    </Link>
+  );
+};
+
+export default ProductCard;
