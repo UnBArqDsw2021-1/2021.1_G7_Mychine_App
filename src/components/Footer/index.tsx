bb005553ca850becca1006428b39d5d3ee94c93d
Sayuck <nobrega.m.roberto@gmail.com>
2021-10-12 18:05:28 -0300

    criando o footer
    Co-authored-by: KessJhones <tavaresjhones14@gmail.com>
    Co-authored-by: Giovannabbottino <giovannabbottino@gmail.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import Logo from '@public/img/logo.svg';
+
 import * as S from './styles';
 
-const Footer = () => <S.Footer>Footer</S.Footer>;
-
+const Footer = () => {
+  return (
+    <S.Footer>
+      <S.Logo>
+        <Image
+          src={Logo}
+          width={50}
+          height={50}
+          objectFit="contain"
+          alt="logo"
+        />
+        <h3>MYCHINE</h3>
+      </S.Logo>
+      <S.Links>
+        <Link href="/" passHref>
+          Termos de uso
+        </Link>
+        <Link href="/" passHref>
+          Documentação
+        </Link>
+        <Link href="/" passHref>
+          Repositório
+        </Link>
+        <Link href="/" passHref>
+          Sobre
+        </Link>
+      </S.Links>
+    </S.Footer>
+  );
+};
 export default Footer;
